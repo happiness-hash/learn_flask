7c4a2159cba8c624a12137b26e84514ce3db7d6b
happiness-hash <xingfurensheng8888@gmail.com>
2025-07-28 10:06:54 +0800

    Add basee template and error template

--- app.py
+++ app.py
@@ -46,11 +46,28 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user = user)
+
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html',user = user,movies = movies)
+    return render_template('index.html',movies = movies )
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    user = User.query.first()
+    return render_template('404.html',user = user),404
+
+# @app.route('/')
+# def index():
+#     user = User.query.first()
+#     movies = Movie.query.all()
+#     return render_template('index.html',user = user,movies = movies)
 
 
 # @app.route('/')
@@ -58,7 +75,8 @@ def index():
 #     return render_template('index.html',name  = name ,movies = movies )
 @app.route('/user/<name>')
 def user_page(name):
-    return f'User: {escape(name)}'
+    # return f'User: {escape(name)}'
+    return render_template("index.html",user = {"name": name})
 
 
 @app.route("/test")
